4e8542bf9a4657ff0920d822343bb7934887dfee
Natalia <icxodnik@gmail.com>
2025-04-27 11:47:07 +0200

    add partially sorted array for tests

--- sort_compare/sort_compare.py
+++ sort_compare/sort_compare.py
@@ -9,12 +9,23 @@ sizes = [100, 1000, 5000, 10000]
 def generate_array(size):
     return [random.randint(0, 100000) for _ in range(size)]
 
-def test_sorting_algorithms():
+def generate_partially_sorted_array(size):
+    return [*range(size//2)] + [random.randint(0, 100000) for _ in range(size//2)]
+
+def test_sorting_algorithms():   
+    print(f"\nНевідсортований масив:")
     for size in sizes:
         arr = generate_array(size)
-
         print(f"\nРозмір масиву: {size}")
+        test_sort(arr)
+    
+    print(f"\nЧастково відсортований масив:")
+    for size in sizes:
+        arr = generate_partially_sorted_array(size)
+        print(f"\nРозмір масиву: {size}")
+        test_sort(arr)
 
+def test_sort(arr):
         time_insertion = timeit.timeit(lambda: insertion_sort(arr.copy()), number=1)
         print(f"Insertion Sort: {time_insertion:.6f} секунд")
 
